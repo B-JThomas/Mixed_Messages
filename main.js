6e588195384607029a1a6b774d8adecb6dfe649a
B-JThomas <bailey.jthomas2@gmail.com>
2024-01-11 15:00:24 +1100

    Movie Quotes added

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
-console.log("Hello World!");
+const movieQuotes = ["May the Force be with you", "There's no place like home.", "I'm the king of the world! ", "Carpe diem. Seize the day, boys. Make your lives extraordinary.",
+"Elementary, my dear Watson.", "It's alive! It's alive!", "My mama always said life was like a box of chocolates. You never know what you're gonna get.",
+"I'll be back", "You're gonna need a bigger boat.", "Here's looking at you, kid.", "My precious.", "Houston, we have a problem",
+"There's no crying in baseball"];
+
+console.log(movieQuotes)
